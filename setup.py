ce246cb07354ad44bb804dda47c871a88ab65d40
potykion <potykion@gmail.com>
2018-07-27 00:43:59 +0300

    0.1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements = [
 
 setuptools.setup(
     name="xlsx_from_json",
-    version="0.1.0",
+    version="0.1.1",
     author="potykion",
     author_email="potykion@gmail.com",
     description="Creates xlsx from json via openpyxl.",
